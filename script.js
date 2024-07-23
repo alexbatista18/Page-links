fd69946f809a9ca722426018046c538883d17c0a
Alex Batista da Costa <alex.batista@dnacenter>
2024-07-23 12:20:25 -0300

    Adicionei o modo dark ao clicar em um botão

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+  const html = document.documentElement
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+  } else {
+    html.classList.add("dark");
+  }
+}
